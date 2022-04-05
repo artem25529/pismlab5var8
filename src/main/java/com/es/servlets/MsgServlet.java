44b411c9fe99d63c4542b329cf86788a4a386224
Artem <martinovich.artem02@mail.ru>
2022-04-05 18:49:53 +0300

    fix

--- src/main/java/com/es/servlets/MsgServlet.java
+++ src/main/java/com/es/servlets/MsgServlet.java
@@ -1,32 +1,23 @@
 package com.es.servlets;
 
+import com.es.service.CardValidator;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @WebServlet("/msg")
 public class MsgServlet extends HttpServlet {
-    private final List<String> messages = new ArrayList(Arrays.asList(
-            "message1", "message2", "message3"
-    ));
+    @EJB
+    private CardValidator validator;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getSession().setAttribute("messages", messages);
-        req.getRequestDispatcher("/WEB-INF/pages/messages.jsp").forward(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        for (int i = 0; i < 2; i++) {
-            messages.set(i, req.getParameter("msg" + i));
-        }
-        req.getRequestDispatcher("/WEB-INF/pages/result.jsp").forward(req, resp);
+        req.setAttribute("res", validator.validate(req.getParameter("date")));
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,2 +1,20 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<c:redirect url="/msg"/>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h2>Check if credit card is valid</h2>
+<form method="get" action="<c:url value="/msg"/>">
+    <label for="date">Input card inspiration date: </label>
+    <input id="date" name="date" value="${param.date}" placeholder="2022-10-05" pattern="\d{4}-\d{2}-\d{2}" size="6px"><br/>
+    <input type="submit" value="Check">
+</form>
+
+<c:if test="${not empty res}">
+    <c:out value="Your card is ${res ? 'valid' : 'unvalid'}"/>
+</c:if>
+
+</body>
+</html>
